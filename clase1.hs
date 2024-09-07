bce11a13d2921140affcae47919a48168df10f75
LuisHerenu11 <ElCondor_1106@hotmail.com>
2024-09-07 06:22:40 -0300

    clase 1

---
+++ clase1.hs
@@ -0,0 +1,9 @@
+suma x y = x + y 
+doble x = 2 * x 
+producto x y = x * y 
+
+cuadrado x = producto x x
+constante8 x = 8
+
+norma :: (Float, Float) -> Float
+norma (x,y) = sqrt (cuadrado x + cuadrado y)
